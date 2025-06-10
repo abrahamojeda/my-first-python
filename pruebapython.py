85a9804d5e829f1027f58122d22c7ade44d07cd4
Alto <abrahamojeda0292@gmail.com>
2025-06-10 12:28:10 -0700

    just text

--- pruebapython.py
+++ pruebapython.py
@@ -1,7 +1,13 @@
+
 def my_funtion():
     a = 5
     b = 3
-    if a > b: print(a) #good practice
+    
+    if a > b: print(a) 
     else: print(b)
-        
+
+def my_function02():
+    pass
+
+
 print(my_funtion())
